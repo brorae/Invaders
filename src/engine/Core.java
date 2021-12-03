9000cc60b483b03f3f9fc958bdd7dc7ba6d39b4c
peanutyumyum <jiwon@LAPTOP-6CVVH7L9.localdomain>
2021-12-03 18:39:52 +0900

    feat: Add Game Summary

--- src/engine/Core.java
+++ src/engine/Core.java
@@ -12,6 +12,7 @@ import screen.GameScreen;
 import screen.HighScoreScreen;
 import screen.ScoreScreen;
 import screen.Screen;
+import screen.SummaryScreen;
 import screen.TitleScreen;
 
 /**
@@ -171,6 +172,15 @@ public final class Core {
 						+ " high score screen at " + FPS + " fps.");
 				returnCode = frame.setScreen(currentScreen);
 				LOGGER.info("Closing high score screen.");
+				break;
+
+			case 5:
+				// Manual.
+				currentScreen = new SummaryScreen(width, height, FPS);
+				LOGGER.info("Starting " + WIDTH + "x" + HEIGHT
+						+ " summary screen at " + FPS + " fps.");
+				returnCode = frame.setScreen(currentScreen);
+				LOGGER.info("Closing manual screen.");
 				break;
 			default:
 				break;
